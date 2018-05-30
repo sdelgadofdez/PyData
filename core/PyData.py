a7f33ed6ab02355539f46b4af526a02e67db1b49
cruzpazv <cruzpazv@gmail.com>
2018-05-30 13:53:35 +0200

    first try

--- core/PyData.py
+++ core/PyData.py
@@ -1,5 +1,8 @@
+import sys
+
 from pyspark import SparkConf, SparkContext
 import time
+
 
 def main(file_name: str) -> None:
     spark_conf = SparkConf()
@@ -9,10 +12,24 @@ def main(file_name: str) -> None:
     logger.LogManager.getLogger("org").setLevel(logger.Level.WARN)
 
     start_computing_time = time.time()
-        airports = spark_context \
-        .textFile(file_name)\
-        .map(lambda line: line.split(","))\
-        .filter(lambda list : list[8] == "\"ES\"")\
-        .map(lambda list: (list[2], 1))\
-        .reduceByKey(lambda x, y: x + y)\
-        .sortBy(lambda pair: pair[1])
+
+    data = spark_context \
+        .textFile(file_name) \
+        .map(lambda line: line.split(",")) \
+        .map(lambda list: (list[5], 1)) \
+        .reduceByKey(lambda x, y: x + y)
+
+    result = data.collect()
+
+    for pair in result:
+        print(pair)
+
+    total_computing_time = time.time() - start_computing_time
+    print("Computing time: ", str(total_computing_time))
+
+    spark_context.stop()
+
+
+if __name__ == "__main__":
+
+    main("U.S._Chronic_Disease_Indicators__CDI_.csv")
